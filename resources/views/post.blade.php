4c538833aac6d6c121e6ff7b42beb06c720ee805
michalm7 <mroczekmichal57@gmail.com>
2022-04-17 09:54:59 +0200

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+
+<body>
+    <article>
+        <?= $post ?>
+    </article>
+    <a href="/">Go back</a>
+</body>
+
+</html>
